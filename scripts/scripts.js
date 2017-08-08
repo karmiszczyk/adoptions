f1bdd658cf4c309e1ca655b6f6163320bc12b0cc
Karolina Miszczyk <karmiszczyk@gmail.com>
2017-08-08 23:54:49 +0200

    create new section- dogInTrouble

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  
+
   $(window).scroll(function () {
-    if ($(window).scrollTop() > 600) {
+    if ($(window).scrollTop() > 500) {
       $('#cssmenu').removeClass('original');
       $('#cssmenu').addClass('fixed');
     }
-    if ($(window).scrollTop() < 600) {
+    if ($(window).scrollTop() < 500) {
       $('#cssmenu').addClass('original');
       $('#cssmenu').removeClass('fixed');
     }
---
+++ scripts/transition.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+$(document).on('click', 'a', function(event){
+	    event.preventDefault();
+	    $('body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 800);
+	});
+});
